b94e92116f3841d8237350aa560c7f108e2aa3eb
thexyman <makdaa1@gmail.com>
2020-06-01 20:52:59 +0100

    added env var

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+const FAVORITE_INSTRUCTOR = process.env.NAME
+
 function sleep(ms) {
+  console.log(`${FAVORITE_INSTRUCTOR} rocks!!`)
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
+  while (true) {
     console.log('Containers rule!');
     await sleep(5000);
   }
